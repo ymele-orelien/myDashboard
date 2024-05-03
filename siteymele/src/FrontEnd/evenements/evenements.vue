6e46aafbe0367557a8cbd695b2b0bb9a4b5850b4
ymele-orelien <orelienymele@gmail.com>
2024-05-03 23:13:44 +0100

    23:13

--- siteymele/src/FrontEnd/evenements/evenements.vue
+++ siteymele/src/FrontEnd/evenements/evenements.vue
@@ -1,6 +1,6 @@
 <template>
-  <section id="team" class="team section-bg" >
-    <div class="container" data-aos="fade-up" v-if="event.length >0">
+  <section id="team" class="team section-bg">
+    <div class="container" data-aos="fade-up">
 
       <div class="section-title">
         <h2>Actualites sur les derniers evenements</h2>
@@ -10,10 +10,10 @@
       </div>
 
       <div class="section_event_slide">
-        <div class="swiper swiper-container">
+        <div class="swiper swiper-container"  v-for="(item, index) in event" :key="index">
           <div class="swiper-wrapper">
 
-            <div class="swiper-slide" v-for="(item, index) in event" :key="index">
+            <div class="swiper-slide">
               <div class="card">
                 <div class="image">
                   <img :src="'http://127.0.0.1:8000/storage/' + item.image" alt="Event Image" >
@@ -22,28 +22,29 @@
                 <div class="content">
                   <a href="#">
                     <span class="title">
-                  {{ item.theme }}
+                      {{ item.theme }}
+
                     </span>
                   </a>
-                  <div class="date"> <i class="fa-solid fa-calendar"></i> {{ item.started }}</div>
+                  <div class="date"> <i class="fa-solid fa-calendar"></i> 15 Jul, 2020</div>
                   <p class="desc">
-                 i{{ item.detail }}
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
+                    dolores, possimus pariatur animi temporibus nesciunt praesentium
                   </p>
 
                   <a class="action" href="#">
-                  
-                    {{ item.user.role==="HOSPITAL"? item.user.name:"ADMINITRATEUR" }}
+                    Find out more
                     <span aria-hidden="true">
-                      
+                      →
                     </span>
                   </a>
                 </div>
               </div>
             </div>
-
-
-
-
+           
+           
+           
+           
           </div>
           <!-- Ajouter la pagination si nécessaire -->
           <div class="swiper-pagination"></div>
@@ -52,11 +53,25 @@
       <div class="d-flex justify-content-center w-100 py-5">
         <router-link to="/event" class="cta">
           <span class="span">VOIR PLUS</span>
-         
+          <span class="second">
+            <svg width="50px" height="20px" viewBox="0 0 66 43" version="1.1" xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink">
+              <g id="arrow" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <path class="one"
+                  d="M40.1543933,3.89485454 L43.9763149,0.139296592 C44.1708311,-0.0518420739 44.4826329,-0.0518571125 44.6771675,0.139262789 L65.6916134,20.7848311 C66.0855801,21.1718824 66.0911863,21.8050225 65.704135,22.1989893 C65.7000188,22.2031791 65.6958657,22.2073326 65.6916762,22.2114492 L44.677098,42.8607841 C44.4825957,43.0519059 44.1708242,43.0519358 43.9762853,42.8608513 L40.1545186,39.1069479 C39.9575152,38.9134427 39.9546793,38.5968729 40.1481845,38.3998695 C40.1502893,38.3977268 40.1524132,38.395603 40.1545562,38.3934985 L56.9937789,21.8567812 C57.1908028,21.6632968 57.193672,21.3467273 57.0001876,21.1497035 C56.9980647,21.1475418 56.9959223,21.1453995 56.9937605,21.1432767 L40.1545208,4.60825197 C39.9574869,4.41477773 39.9546013,4.09820839 40.1480756,3.90117456 C40.1501626,3.89904911 40.1522686,3.89694235 40.1543933,3.89485454 Z"
+                  fill="#FFFFFF"></path>
+                <path class="two"
+                  d="M20.1543933,3.89485454 L23.9763149,0.139296592 C24.1708311,-0.0518420739 24.4826329,-0.0518571125 24.6771675,0.139262789 L45.6916134,20.7848311 C46.0855801,21.1718824 46.0911863,21.8050225 45.704135,22.1989893 C45.7000188,22.2031791 45.6958657,22.2073326 45.6916762,22.2114492 L24.677098,42.8607841 C24.4825957,43.0519059 24.1708242,43.0519358 23.9762853,42.8608513 L20.1545186,39.1069479 C19.9575152,38.9134427 19.9546793,38.5968729 20.1481845,38.3998695 C20.1502893,38.3977268 20.1524132,38.395603 20.1545562,38.3934985 L36.9937789,21.8567812 C37.1908028,21.6632968 37.193672,21.3467273 37.0001876,21.1497035 C36.9980647,21.1475418 36.9959223,21.1453995 36.9937605,21.1432767 L20.1545208,4.60825197 C19.9574869,4.41477773 19.9546013,4.09820839 20.1480756,3.90117456 C20.1501626,3.89904911 20.1522686,3.89694235 20.1543933,3.89485454 Z"
+                  fill="#FFFFFF"></path>
+                <path class="three"
+                  d="M0.154393339,3.89485454 L3.97631488,0.139296592 C4.17083111,-0.0518420739 4.48263286,-0.0518571125 4.67716753,0.139262789 L25.6916134,20.7848311 C26.0855801,21.1718824 26.0911863,21.8050225 25.704135,22.1989893 C25.7000188,22.2031791 25.6958657,22.2073326 25.6916762,22.2114492 L4.67709797,42.8607841 C4.48259567,43.0519059 4.17082418,43.0519358 3.97628526,42.8608513 L0.154518591,39.1069479 C-0.0424848215,38.9134427 -0.0453206733,38.5968729 0.148184538,38.3998695 C0.150289256,38.3977268 0.152413239,38.395603 0.154556228,38.3934985 L16.9937789,21.8567812 C17.1908028,21.6632968 17.193672,21.3467273 17.0001876,21.1497035 C16.9980647,21.1475418 16.9959223,21.1453995 16.9937605,21.1432767 L0.15452076,4.60825197 C-0.0425130651,4.41477773 -0.0453986756,4.09820839 0.148075568,3.90117456 C0.150162624,3.89904911 0.152268631,3.89694235 0.154393339,3.89485454 Z"
+                  fill="#FFFFFF"></path>
+              </g>
+            </svg>
+          </span>
         </router-link>
       </div>
     </div>
-    <div v-else></div>
   </section>
 </template>
 
@@ -125,8 +140,8 @@ export default {
 
 <style scoped>
 /*--------------------------------------------------------------
-  # Team
-  --------------------------------------------------------------*/
+# Team
+--------------------------------------------------------------*/
 h2,
 p {
   color: #000 !important;
@@ -134,7 +149,6 @@ p {
 
 .team {
   padding-top: 100px;
-  background: ghostwhite;
 }
 
 .swiper-container {
@@ -145,7 +159,6 @@ p {
 .section_event_slide {
   position: relative;
   width: 100%;
-
   min-height: 80vh;
   display: flex;
   justify-content: center;
@@ -167,44 +180,25 @@ p {
   background: #fff;
   background-position: center;
   background-size: cover;
-  width: 550px;
-  height: 650px;
+  width: 350px;
+  height: auto;
 }
 
 /* card */
 .card {
-  max-width: 100%;
+  max-width: 340px;
   border-radius: 0.5rem;
-  border-radius: 8px !important;
-
+  background-color: #fff;
   box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.05);
   border: 1px solid transparent;
 }
 
-.date {
-  background: #00002c;
-  width: 120px;
-  border-radius: 5px;
-  padding: 0.8rem;
-  color: #fff;
-}
-
 .card a {
-  text-decoration: none;
-  align-items: center;
-  text-align: center;
+  text-decoration: none
 }
 
 .content {
-  padding: 1.5rem;
-  position: relative;
-  margin-top: 20%;
-  width: 70%;
-  background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
-  backdrop-filter: blur(10px);
-  -webkit-backdrop-filter: blur(10px);
-  border: 1px solid rgba(255, 255, 255, 0);
-  box-shadow: 0px 0px 32px 0 rgba(0, 0, 0, 0.1);
+  padding: 1.1rem;
 }
 
 .image {
@@ -216,13 +210,12 @@ p {
 
 .image img {
   width: 100%;
-  height: 650px;
+  height: 100%;
   object-fit: cover;
-  border-radius: 10px;
 }
 
 .title {
-  color: #fff;
+  color: #111827;
   font-size: 1.125rem;
   line-height: 1.75rem;
   font-weight: 600;
--- siteymele/src/main.js
+++ siteymele/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue';
 import router from './router';
 import VueToast from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-sugar.css'; // Assurez-vous d'importer le CSS
-
+import AOS from 'aos';
+import 'aos/dist/aos.css' 
 const app = createApp(App);
 
 // Utilisation de vue-toast-notification comme propriété globale
@@ -11,6 +12,13 @@ app.use(VueToast);
 
 // Utilisation du routeur
 app.use(router);
+
+app.config.globalProperties.$aos = AOS.init({
+    duration: 1000,
+    easing: 'ease-in-out',
+    once: true,
+    mirror: false
+  });
 
 // Montage de l'application sur l'élément HTML avec l'ID 'app'
 app.mount('#app');
